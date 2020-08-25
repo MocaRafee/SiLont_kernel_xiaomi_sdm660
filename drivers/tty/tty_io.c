02be8477225d8037151322dd769b6324628ccb70
Reinazhard <muh.alfarozy@gmail.com>
2020-08-25 16:06:21 +0700

    drivers: nuke undefined reference
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -1348,11 +1348,8 @@ static void pty_line_name(struct tty_driver *driver, int index, char *p)
  */
 static ssize_t tty_line_name(struct tty_driver *driver, int index, char *p)
 {
-	if (driver->flags & TTY_DRIVER_UNNUMBERED_NODE)
-		return sprintf(p, "%s", driver->name);
-	else
-		return sprintf(p, "%s%d", driver->name,
-			       index + driver->name_base);
+	return sprintf(p, "%s%d", driver->name,
+			    index + driver->name_base);
 }
 
 /**
